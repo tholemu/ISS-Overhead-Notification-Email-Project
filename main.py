e1202f52589b0a16cf151e46faaf11eb2ffe8359
bowler <m.bowler@f5.com>
2023-03-26 20:14:29 -0500

    initial commit to tholemu-dev

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ from datetime import datetime
 import smtplib
 import time
 
-MY_EMAIL = "name@yahoo.com" #Enter your valid email address
+MY_EMAIL = "username" #Enter your valid email address
 MY_PASSWORD = "password" #Enter your email password
 MY_LAT = 38.627003 # Your latitude
 MY_LONG = -90.199402 # Your longitude
+
+# Run the script, gather the current coordinates
+# then update the following accordingly to force
+# success
+MY_LAT = 5.1907
+MY_LONG = 177.9746
 
 
 #Function for findout out if the ISS is close to my current position
@@ -18,12 +24,16 @@ def is_iss_overhead():
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])
 
+    print(f"iss_latitude: {iss_latitude}")
+    print(f"iss_longitude: {iss_longitude}")
+
     #Your position is within +5 or -5 degrees of the ISS position.
     if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
         return True
 
 #Function for finding out if it is currently dark
 def is_night():
+    print("Begin is_night()")
     parameters = {
         "lat": MY_LAT,
         "lng": MY_LONG,
@@ -36,10 +46,17 @@ def is_night():
     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
     sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
+    print(f"sunrise: {sunrise}")
+    print(f"sunset: {sunset}")
+
     time_now = datetime.now().hour
+
+    print(f"time_now: {time_now}")
 
     if time_now >= sunset or time_now <= sunrise:
         return True
+    
+    return False
 
 #While loop for determining if ISS is overhead and if its dark, then send yourself an email to look up in the sky
 #Make sure you've got the correct smtp address for your email provider:
@@ -48,9 +65,8 @@ def is_night():
 #Outlook: outlook.office365.com
 #Yahoo: smtp.mail.yahoo.com
 while True:
-    time.sleep(60)
     if is_iss_overhead() and is_night():
-        connection = smtplib.SMTP("smtp.mail.yahoo.com")
+        connection = smtplib.SMTP("smtp.gmail.com")
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
         connection.sendmail(
@@ -58,6 +74,8 @@ while True:
             to_addrs=MY_EMAIL,
             msg="Subject:Look Up!\n\nThe ISS is above you in the sky!"
         )
+    print("Retrying again in 60 sec")
+    time.sleep(60)
 
 
 
